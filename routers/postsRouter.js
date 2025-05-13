7733876f26dacc0ee0fcd6e7209f04c0ebcac27c
bradyshehu <shehu.brady@gmail.com>
2025-05-13 17:26:14 +0200

    add all CRUD operations

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -6,17 +6,40 @@ const posts = require("../posts");
 // INDEX
 
 router.get("", (req, res) => {
-  res.json(posts);
-  console.log("Lettura di tutti i post");
+  res.json({
+    description: "Lista di tutti i post",
+    data: posts,
+  });
 });
 
 // SHOW
 
 router.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const currentPost = posts.find((post) => post.id == id);
-  res.json(currentPost);
-  console.log("Lettura del post " + id);
+  const currentPost = posts.posts.find((post) => post.id == id);
+  res.json({
+    description: "Lettura del post " + id,
+    data: currentPost,
+  });
+});
+
+router.post("", (req, res) => {
+  res.send("Creazione di un nuovo post");
+});
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const currentPost = posts.posts.find((post) => post.id == id);
+  res.send("Sostituzione del post " + id);
+});
+router.patch("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const currentPost = posts.posts.find((post) => post.id == id);
+  res.send("Modifica del post" + id);
+});
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const currentPost = posts.posts.find((post) => post.id == id);
+  res.send("Cancellazione del post " + id);
 });
 
 module.exports = router;
